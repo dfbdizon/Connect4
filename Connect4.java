1fb2c0cd6ace65cb1d7631eefd73232f17b7098f
oliviadmtr <olivia.demetria@gmail.com>
2015-05-31 20:15:50 +0800

    My Changes

--- Connect4.java
+++ Connect4.java
@@ -24,4 +24,24 @@ public class Connect4{
 		board.put(6, "000000");
 		board.put(7, "000000");
 	}
+
+
+	// Heuristic 1?
+	// Count number of possible 4 in a rows that each player can still make in the current state
+	
+	// Heuristic 2?
+	// Score = My Material - Opponent's Material
+	// Material = count of all contiguous lines of pieces that a player has, WEIGHTED, so 1 in a row, .1 points, etc.
+	// Consider only ung may open positions pa
+
+
+	private static int getStateScore(){
+
+	}
+
+
+
+
+
+
 }
