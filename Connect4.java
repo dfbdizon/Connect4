9ca461308af652f532ee39a3927fcb91cbdde039
oliviadmtr <olivia.demetria@gmail.com>
2015-06-01 03:16:49 +0800

    Eval function changes sa Connect 4

--- Connect4.java
+++ Connect4.java
@@ -101,11 +101,374 @@ public class Connect4{
 	// Material = count of all contiguous lines of pieces that a player has, WEIGHTED, so 1 in a row, .1 points, etc.
 	// Consider only ung may open positions pa
 
+	//Dapat may magssave ng stateScores ni player 1 and 2 doon sa currentState
 
-	private static int getStateScore(){
+
+	private static double getStateScore(char player, int row, int col, double myMaterial, double oppMaterial){
+		String currentColumn;
+		char currentPiece;
+		double retDouble = myMaterial - oppMaterial;
+		return retDouble;
+	}
+
+	private static double updateMaterial(char player, int row, int col, double prevMaterial){
+		double retDouble = prevMaterial;
+		retDouble += checkHowManyInARow(player, row, col);
+		return retDouble;
+	}
+	//do I count ung rows na wala naman na pagasa makabuo pa ng 4? this implementation, NO
+	//cinocount ko rin ung doubles, IE 1 in a row sya, pero pwede ring 2 in a row pala etc DUPLICATES, which makes sense since higher chances of winning naman talaga if maraming possible successes sa move na un
+
+	private static double checkHowManyInARow(char player, int row, int col){
+		double retDouble = 0;
+
+		char left, right, up, down, upRight, upLeft, downRight, downLeft;
+		char left1, right1, up1, down1, upRight1, upLeft1, downRight1, downLeft1;
+		char left2, right2 up2, down2, upRight2, upLeft2, downRight2, downLeft2;
+		
+		left = getLeft(col, row);
+		right = getRight(col, row);
+		up = getUp(col, row);
+		down = getDown(col, row);
+		upRight = getUpRight(col, row);
+		upLeft = getUpLeft(col, row);
+		downRight = getDownRight(col, row);
+		downLeft = getDownLeft(col, row);
+
+		//get left nung nasa taas
+		left1 = getLeft(col-1, row);
+		right1 = getRight(col+1, row);
+		up1 = getUp(col, row+1);
+		down1 = getDown(col, row-1);
+		upRight1 = getUpRight(col+1, row+1);
+		upLeft1 = getUpLeft(col-1, row+1);
+		downRight1 = getDownRight(col+1, row-1);
+		downLeft1 = getDownLeft(col-1, row-1);
+
+		left2 = getLeft(col-2, row);
+		right2 = getRight(col+2, row);
+		up2 = getUp(col, row+2);
+		down2 = getDown(col, row-2);
+		upRight2 = getUpRight(col+2, row+2);
+		upLeft2 = getUpLeft(col-2, row+2);
+		downRight2 = getDownRight(col+2, row-2);
+		downLeft2 = getDownLeft(col-2, row-2);
+
+		//HORIZONTAL
+		//check if open ung left
+		if(left == '0'){ // 0|P
+			//check if open ung right
+			if(right == '0'){ //0P|0
+				//check if open ung left left or right right
+				if(right1 == '0' || left1 == '0'){ //0P0|0 or 0|0P0
+					retDouble += .1;
+				}
+			}
+			//if di open ung right, check if piece nya un 
+			else if(right == player){ //0P|P
+				//check if open ung left1
+				if(left1 == '0'){ //0|0PP
+					retDouble += .2;
+				}
+				else if(right1 == '0'){//0PP|0
+					retDouble += .2;
+				}
+				else if(right1 == player){//0PP|P
+					retDouble += .3;
+					if(right2 == '0'){//0PPP|0
+						retDouble += .3;
+					}
+					else if(right2 == player){//0PPP|P
+						retDouble += .10; //perfect 4 in a row
+					}
+				}
+			}
+			else{ //0P|sumth
+				if(left1 == '0' && left2 == '0'){ //00|0P
+					retDouble += .1;
+				}
+			}
+		}
+		else if(left == player){ //P|P
+			if(right == '0'){ //PP|0
+				if(right1 == '0' || left1 == '0'){ //0|PP0 or PP0|0
+					retDouble += .2;
+				}
+				else if(left1 == player){ //P|PP0
+					retDouble += .3; 
+					if(left2 == '0'){ //0|PPP0
+						retDouble += .3;
+					}
+					else if(left2 == player){ //P|PPP0
+						retDouble += .10;
+					}
+				}
+			}
+			else if(right == player){ //PP|P
+				if(right1 == '0'){//PPP|0
+					retDouble += .3;
+				}
+				else if(right1 == player){ // PPP|P
+					retDouble += .10;
+				}
+				if(left1 == '0'){ //0|PPP
+					retDouble += .3;
+				}
+				else if(left1 == player){ // PPP|P
+					retDouble += .10;
+				}
+			}
+		}
+
+		//VERTICAL
+		//parang ung nasa taas lang, left lang ay up, right ay down
+		if(up == '0'){ // 0|P
+			//check if open ung right
+			if(down == '0'){ //0P|0
+				//check if open ung left left or right right
+				if(down1 == '0' || up1 == '0'){ //0P0|0 or 0|0P0
+					retDouble += .1;
+				}
+			}
+			//if di open ung right, check if piece nya un 
+			else if(down == player){ //0P|P
+				//check if open ung left1
+				if(up1 == '0'){ //0|0PP
+					retDouble += .2;
+				}
+				else if(down1 == '0'){//0PP|0
+					retDouble += .2;
+				}
+				else if(down1 == player){//0PP|P
+					retDouble += .3;
+					if(down2 == '0'){//0PPP|0
+						retDouble += .3;
+					}
+					else if(down2 == player){//0PPP|P
+						retDouble += .10; //perfect 4 in a row
+					}
+				}
+			}
+			else{ //0P|sumth
+				if(up1 == '0' && up2 == '0'){ //00|0P
+					retDouble += .1;
+				}
+			}
+		}
+		else if(up == player){ //P|P
+			if(down == '0'){ //PP|0
+				if(down1 == '0' || up1 == '0'){ //0|PP0 or PP0|0
+					retDouble += .2;
+				}
+				else if(up1 == player){ //P|PP0
+					retDouble += .3; 
+					if(up2 == '0'){ //0|PPP0
+						retDouble += .3;
+					}
+					else if(up2 == player){ //P|PPP0
+						retDouble += .10;
+					}
+				}
+			}
+			else if(down == player){ //PP|P
+				if(down1 == '0'){//PPP|0
+					retDouble += .3;
+				}
+				else if(down1 == player){ // PPP|P
+					retDouble += .10;
+				}
+				if(up1 == '0'){ //0|PPP
+					retDouble += .3;
+				}
+				else if(up1 == player){ // PPP|P
+					retDouble += .10;
+				}
+			}
+		}
+
+		//DIAGONAL 1
+		//parang ganun ulit except, up is upLeft, down = downRight
+		if(upLeft == '0'){ // 0|P
+			//check if open ung right
+			if(downRight == '0'){ //0P|0
+				//check if open ung left left or right right
+				if(downRight1 == '0' || upLeft1 == '0'){ //0P0|0 or 0|0P0
+					retDouble += .1;
+				}
+			}
+			//if di open ung right, check if piece nya un 
+			else if(downRight == player){ //0P|P
+				//check if open ung left1
+				if(upLeft1 == '0'){ //0|0PP
+					retDouble += .2;
+				}
+				else if(downRight1 == '0'){//0PP|0
+					retDouble += .2;
+				}
+				else if(downRight1 == player){//0PP|P
+					retDouble += .3;
+					if(downRight2 == '0'){//0PPP|0
+						retDouble += .3;
+					}
+					else if(downRight2 == player){//0PPP|P
+						retDouble += .10; //perfect 4 in a row
+					}
+				}
+			}
+			else{ //0P|sumth
+				if(upLeft1 == '0' && upLeft2 == '0'){ //00|0P
+					retDouble += .1;
+				}
+			}
+		}
+		else if(upLeft == player){ //P|P
+			if(downRight == '0'){ //PP|0
+				if(downRight1 == '0' || upLeft1 == '0'){ //0|PP0 or PP0|0
+					retDouble += .2;
+				}
+				else if(upLeft1 == player){ //P|PP0
+					retDouble += .3; 
+					if(upLeft2 == '0'){ //0|PPP0
+						retDouble += .3;
+					}
+					else if(upLeft2 == player){ //P|PPP0
+						retDouble += .10;
+					}
+				}
+			}
+			else if(downRight == player){ //PP|P
+				if(downRight1 == '0'){//PPP|0
+					retDouble += .3;
+				}
+				else if(downRight1 == player){ // PPP|P
+					retDouble += .10;
+				}
+				if(upLeft1 == '0'){ //0|PPP
+					retDouble += .3;
+				}
+				else if(upLeft1 == player){ // PPP|P
+					retDouble += .10;
+				}
+			}
+		}
+
+		//DIAGONAL 2
+		//parang ganun ulit except, up is upRight, down = downLeft
+		if(upRight == '0'){ // 0|P
+			//check if open ung right
+			if(downLeft == '0'){ //0P|0
+				//check if open ung left left or right right
+				if(downLeft1 == '0' || upRight1 == '0'){ //0P0|0 or 0|0P0
+					retDouble += .1;
+				}
+			}
+			//if di open ung right, check if piece nya un 
+			else if(downLeft == player){ //0P|P
+				//check if open ung left1
+				if(upRight1 == '0'){ //0|0PP
+					retDouble += .2;
+				}
+				else if(downLeft1 == '0'){//0PP|0
+					retDouble += .2;
+				}
+				else if(downLeft1 == player){//0PP|P
+					retDouble += .3;
+					if(downLeft2 == '0'){//0PPP|0
+						retDouble += .3;
+					}
+					else if(downLeft2 == player){//0PPP|P
+						retDouble += .10; //perfect 4 in a row
+					}
+				}
+			}
+			else{ //0P|sumth
+				if(upRight1 == '0' && upRight2 == '0'){ //00|0P
+					retDouble += .1;
+				}
+			}
+		}
+		else if(upRight == player){ //P|P
+			if(downLeft == '0'){ //PP|0
+				if(downLeft1 == '0' || upRight1 == '0'){ //0|PP0 or PP0|0
+					retDouble += .2;
+				}
+				else if(upRight1 == player){ //P|PP0
+					retDouble += .3; 
+					if(upRight2 == '0'){ //0|PPP0
+						retDouble += .3;
+					}
+					else if(upRight2 == player){ //P|PPP0
+						retDouble += .10;
+					}
+				}
+			}
+			else if(downLeft == player){ //PP|P
+				if(downLeft1 == '0'){//PPP|0
+					retDouble += .3;
+				}
+				else if(downLeft1 == player){ // PPP|P
+					retDouble += .10;
+				}
+				if(upRight1 == '0'){ //0|PPP
+					retDouble += .3;
+				}
+				else if(upRight1 == player){ // PPP|P
+					retDouble += .10;
+				}
+			}
+		}
+
+		return retDouble;
 
 	}
 
+	private static char getLeft(int col, int row){
+		char retChar = '\0'; //null
+		if(col != 1) retChar = board.get(col-1).charAt(row);
+		return retChar;
+	}
+
+	private static char getRight(int col, int row){
+		char retChar = '\0';
+		if(col != 7) retChar = board.get(col+1).charAt(row);
+		return retChar;
+	}
+
+	private static char getUp(int col, int row){
+		char retChar = '\0';
+		if(row != 5 ) retChar = board.get(col).charAt(row+1);
+		return retChar;
+	}
+
+	private static char getDown(int col, int row){
+		char retChar = '\0';
+		if(row != 0) retChar = board.get(col).charAt(row-1);
+		return retChar;
+	}
+
+	private static char getUpLeft(int col, int row){
+		char retChar = '\0';
+		if(row != 5 && col != 1) retChar = board.get(col-1).charAt(row+1);
+		return retChar;
+	}
+
+	private static char getUpRight(int col, int row){
+		char retChar = '\0';
+		if(row != 5 && col != 7) retChar = board.get(col+1).charAt(row+1);
+		return retChar;
+	}
+
+	private static char getDownLeft(int col, int row){
+		char retChar = '\0';
+		if(row != 0 && col != 1) retChar = board.get(col-1).charAt(row-1);
+		return retChar;
+	}
+
+	private static char getDownRight(int col, int row){
+		char retChar = '\0';
+		if(row != 0 && col != 7) retChar = baord.get(col+1).charAt(row-1);
+		return retChar;
+	}
 
 
 
