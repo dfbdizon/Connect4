6096e4c5582b349ce0ec7885deaf6d648fedb09b
dfbdizon <diana.dizon22@gmail.com>
2015-05-31 18:12:37 +0800

    Initialized Connect4.java

---
+++ Connect4.java
@@ -0,0 +1,18 @@
+public class Connect4{
+	HashMap<Integer, String> = new HashMap<Integer, String>(42);
+	public static void main(String [] args){
+
+
+
+
+	}
+	private static boolean hasWinner(int col){
+
+
+
+
+
+
+		
+	}
+}
