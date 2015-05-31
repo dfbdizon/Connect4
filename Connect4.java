c77fc5699780a6bc2836a4626bb9f7545ad7f211
Tetey <aglagunda@up.edu.ph>
2015-06-01 03:15:59 +0800

    Added minimax method

--- Connect4.java
+++ Connect4.java
@@ -5,6 +5,7 @@ public class Connect4{
 	public static void main(String [] args){
 		initializeBoard();
 	}
+	@SuppressWarnings("unused")
 	private static boolean hasWinner(int col, int row, char player){
 		if(checkHor(col, row, player) || checkVer(col, row, player) || checkDiagonalLeft(col, row, player) || checkDiagonalRight(col, row, player))
 			return true;
@@ -91,4 +92,53 @@ public class Connect4{
 		board.put(6, "000000");
 		board.put(7, "000000");
 	}
+	
+	static double getStateScore2(char player, int row, int col){
+		return 0;
+	}
+	
+	int MinMax(int col, int row, char player) {
+		Tree mainTree = new Tree(col, row, player, new Node(board, 0));
+		return MaxMove (col, row, player, mainTree);
+	}
+	 
+	/*int MinMaxIterative(int col, int row, char player){
+		boolean isMax = true;
+		while(!hasWinner(col, row, player)){
+			if(isMax){
+				
+				isMax=!isMax;
+			}else{
+				
+				isMax=!isMax;
+			}
+		}
+	}*/
+	int MaxMove(int col, int row, char player, Tree tree) {
+		if (hasWinner(col, row, player)) {
+			return 9;//EvalGameState(config);
+		}
+		else {
+			Node bestMove = tree.getChild(0);//initialize best move
+			Node currMove = tree.getChild(1);
+			Tree currTree = new Tree(col, row, '2', currMove);
+			for(int i = 0; i <= tree.children.size()-1; i++) {//iterate on each move
+				if (getStateScore2('2', row, tree.children.indexOf(MinMove(col, row, '2', currTree))) > getStateScore2('2', row, tree.children.indexOf(bestMove))) 
+					bestMove = currMove;
+			}
+			return tree.children.indexOf(bestMove)+1;
+		}
+	}
+	 
+	int MinMove(int col, int row, char player, Tree tree) {
+		Node bestMove = tree.getChild(1);
+		Node currMove = tree.getChild(0);//initialize best move
+		Tree currTree = new Tree(col, row, '1', currMove);
+		for(int i = 0; i <= tree.children.size()-1; i++) {//iterate on all moves
+			if (getStateScore2('2', row, tree.children.indexOf(MaxMove(col, row, '1', currTree))) > getStateScore2('2', row, tree.children.indexOf(bestMove))) 
+				bestMove = currMove;
+		}
+		return tree.children.indexOf(bestMove)+1;
+	}
+	
 }
--- Node.java
+++ Node.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 class Node{
 	HashMap<Integer, String> config;
 	double score;
@@ -10,5 +12,14 @@ class Node{
 	}
 	public double getScore(){
 		return score;
+	}
+	public HashMap<Integer, String> move(int col){
+		HashMap<Integer, String> moved = config;
+		String currConfig = config.get(col);
+		System.out.println("before: " + currConfig);
+		currConfig = currConfig.replaceFirst("0", "1");
+		System.out.println("after: " + currConfig);
+		moved.replace(col, currConfig);
+		return moved;
 	}
 }
