a6eee36764fbe1937eee3ff0159b39229935b7db
dfbdizon <diana.dizon22@gmail.com>
2015-05-31 18:34:05 +0800

    Initialized board

--- Connect4.java
+++ Connect4.java
@@ -1,7 +1,7 @@
 public class Connect4{
-	HashMap<Integer, String> = new HashMap<Integer, String>(42);
+	HashMap<Integer, String> board = new HashMap<Integer, String>(42);
 	public static void main(String [] args){
-
+		initializeBoard();
 
 
 
@@ -13,6 +13,15 @@ public class Connect4{
 
 
 
-		
+
+	}
+	private static void initializeBoard(){
+		board.put(1, "000000");
+		board.put(2, "000000");
+		board.put(3, "000000");
+		board.put(4, "000000");
+		board.put(5, "000000");
+		board.put(6, "000000");
+		board.put(7, "000000");
 	}
 }
