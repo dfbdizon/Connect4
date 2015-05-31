d57666067c573c557b89177e59a29ed5ccd00d20
dfbdizon <diana.dizon22@gmail.com>
2015-05-31 21:48:36 +0800

    Created Tree.java and Node.java

---
+++ Tree.java
@@ -0,0 +1,22 @@
+class Tree{
+	ArrayList<Node> children = new ArrayList<Node>();
+	Node root;
+	public void setRootNode(Node node){
+		root = node;
+	}
+	public void addChild(Node node){
+		children.add(node);
+	}
+	public void removeChild(int index){
+		children.remove(index);
+	}
+	public Node returnRoot(){
+		return root;
+	}
+	public ArrayList<Node> getChildren(){
+		return children;
+	}
+	public Node getChild(int index){
+		return (children.get(index));
+	}
+}
